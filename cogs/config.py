bd62cb498eaeb9ea83f95ad9338e1c624db7f514
Wenis77 <65354715+Wenis77@users.noreply.github.com>
2021-02-21 18:04:01 -0500

    Set your server mute and dungeon roles

--- cogs/config.py
+++ cogs/config.py
@@ -23,6 +23,30 @@ class ConfigCog(commands.Cog):
     async def not_blocked(ctx):
         return GF.check_block(ctx.author.id, ctx.command.name)
 
+    async def create_command_check(self, type, guild, type_id, ctx):
+        conn = sqlite3.connect(db_path)
+        c = conn.cursor()
+
+        c.execute("SELECT * FROM command_check WHERE server_id=? and type=?", (guild.id, type,))
+
+        row = c.fetchone()
+
+        if row:
+            c.execute("UPDATE command_check SET type_id=? WHERE server_id=? AND type=?", (type_id, guild.id, type,))
+            conn.commit()
+            conn.close()
+            return
+
+        default = get(guild.roles, name="@everyone")
+        perms = default.permissions
+        role = await guild.create_role(name=type, permissions=perms)
+
+
+        c.execute("INSERT INTO command_check (type_id, server_id, type) VALUES (?,?,?)", (role.id, guild.id, type,))
+        conn.commit()
+        conn.close()
+
+
     @commands.group(pass_context=True)
     @commands.check(has_moderator)
     @commands.check(not_blocked)
@@ -43,6 +67,25 @@ class ConfigCog(commands.Cog):
         conn.commit()
 
         await ctx.send(f"Your prefix has been set to `{prefix}`")
+
+    @set.command(pass_context=True)
+    @commands.check(has_moderator)
+    @commands.check(not_blocked)
+    async def muted(self, ctx, role: discord.Role):
+        await ctx.send("?")
+        try:
+            await self.create_command_check("Muted", ctx.guild, role.id, ctx)
+            await ctx.send(f"Set muted role to {role.mention}, make sure to adjust the perms!")
+        except Exception as e:
+            await ctx.send(e)
+
+    @set.command(pass_context=True)
+    @commands.check(has_moderator)
+    @commands.check(not_blocked)
+    async def dungeoned(self, ctx, role: discord.Role):
+        await self.create_command_check("Dungoned", ctx.guild, role.id)
+        await ctx.send(f"Set dungeond role to {role.mention}, make sure to adjust the perms!")
+
 
 
 
